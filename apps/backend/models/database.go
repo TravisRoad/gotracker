fa331cdc712c0d3f0ec4c7a00069f28b93af31c7
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-07-11 22:50:26 +0800

    增加了分页插件

--- apps/backend/models/database.go
+++ apps/backend/models/database.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -21,4 +24,24 @@ func migrate(db *gorm.DB) {
 		&User{}, &Review{}, &Collection{}, &Seen{},
 		&Metadata{}, &Keyword{},
 		&Movie{}, &Book{}, &Game{}, &Manga{}, &Policy{})
+}
+
+func Paginate(c *gin.Context) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page, _ := strconv.Atoi(c.Query("page"))
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize, _ := strconv.Atoi(c.Query("page_size"))
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
 }

00d29a5dddceef90e71cd5314d7bbe4a6c847c47
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-07-11 22:50:10 +0800

    添加了常量

---
+++ apps/backend/constants/constants.go
@@ -0,0 +1,20 @@
+package constants
+
+const (
+	WANT     = 0
+	WATCHED  = 1
+	WATCHING = 2
+	ONHOLD   = 3
+	DROPPED  = 4
+)
+
+const (
+	NOTSEEN = 1
+
+	USER         = 100
+	UNAUTHORIZED = 101
+	USERNOTEXIST = 102
+
+	BODY      = 200
+	BODYERROR = 201
+)
